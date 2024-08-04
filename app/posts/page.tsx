570b4ffac2a2644cf3861dc8a3505989cb64da0e
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-08-04 13:43:21 -0700

    Topic Linking completed on Aug 04, 24

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,7 +1,9 @@
+'use client';
+
 import styles from "./page.module.css";
 import { Inter } from "next/font/google";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +14,9 @@ export default function Posts() {
       <div className={styles.center}><h1>Post</h1></div>
       <Link href="/">Back to Home</Link>  
 
-      <button>Back to Home</button>   
+      {/* <button type="button" onClick={() => router.push('/')}>
+        Back to Home
+      </button> */}
     </main>
   );
 }
