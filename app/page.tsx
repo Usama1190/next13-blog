b1a60f9fd96e0ad978f8b8954e03fbea3390c7c2
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-08-06 00:42:30 -0700

    Keep learning on Aug 06, 24

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,14 @@ import styles from "./page.module.css";
 import { Inter } from "next/font/google";
 import Link from 'next/link'
 
+import { posts } from "../data/posts";
+
+type Post = {
+  id: string,
+  title: string,
+  date: string
+}
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -11,6 +19,14 @@ export default function Home() {
       <div className={styles.center}>
         <h2>Home</h2>
       </div> 
+      <div>{posts.map(({id, title, date}: Post) => (
+        <>
+          <Link href="/posts">{title}</Link>
+          <br />
+          <p>{date}</p>
+          <br />
+        </>
+      ))}</div>
       <Link href="/posts">Posts</Link>
       <Link href="/account">Accounts</Link>
     </main>
